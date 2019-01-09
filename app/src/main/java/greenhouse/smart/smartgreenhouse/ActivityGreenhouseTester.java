f32ab1f60f8fed7363653d7518a82582c722891b
Felipe Carvalho <38670592+FelipeSetembrino@users.noreply.github.com>
2019-01-09 10:16:25 +0000

    Correction

--- app/src/main/java/greenhouse/smart/smartgreenhouse/ActivityGreenhouseTester.java
+++ app/src/main/java/greenhouse/smart/smartgreenhouse/ActivityGreenhouseTester.java
@@ -28,19 +28,19 @@ public class ActivityGreenhouseTester extends AppCompatActivity {
         Switch switchExaustor = (Switch) exaustorLayout.findViewById(R.id.switchEquipOnOff);
         //Iluminação
         LinearLayout iluminacaoLayout = (LinearLayout) findViewById(R.id.iluminacaoItem);
-        TextView textViewIluminacao = (TextView) exaustorLayout.findViewById(R.id.textEquipName);
-        textViewExaustor.setText("Iluminação");
-        Switch switchIluminacao = (Switch) exaustorLayout.findViewById(R.id.switchEquipOnOff);
+        TextView textViewIluminacao = (TextView) iluminacaoLayout.findViewById(R.id.textEquipName);
+        textViewIluminacao.setText("Iluminação");
+        Switch switchIluminacao = (Switch) iluminacaoLayout.findViewById(R.id.switchEquipOnOff);
         //Pump
         LinearLayout pumpLayout = (LinearLayout) findViewById(R.id.pumpItem);
-        TextView textViewPump = (TextView) exaustorLayout.findViewById(R.id.textEquipName);
-        textViewExaustor.setText("Bomba irrigação");
-        Switch switchPump = (Switch) exaustorLayout.findViewById(R.id.switchEquipOnOff);
+        TextView textViewPump = (TextView) pumpLayout.findViewById(R.id.textEquipName);
+        textViewPump.setText("Bomba irrigação");
+        Switch switchPump = (Switch) pumpLayout.findViewById(R.id.switchEquipOnOff);
         //Umidificador
         LinearLayout umidificadorLayout = (LinearLayout) findViewById(R.id.umidificadorItem);
-        TextView textViewUmidificador = (TextView) exaustorLayout.findViewById(R.id.textEquipName);
-        textViewExaustor.setText("Umidificador");
-        Switch switchUmidificador = (Switch) exaustorLayout.findViewById(R.id.switchEquipOnOff);
+        TextView textViewUmidificador = (TextView) umidificadorLayout.findViewById(R.id.textEquipName);
+        textViewUmidificador.setText("Umidificador");
+        Switch switchUmidificador = (Switch) umidificadorLayout.findViewById(R.id.switchEquipOnOff);
 
     }
 }
